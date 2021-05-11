8c8339991cea96511c2794f1fb3a80e668295b05
Relag <44652125+Relag@users.noreply.github.com>
2021-05-11 18:34:21 -0400

    Outlined PlayerShark script
    Playershark has TakeDamage, HealDamage, LevelUp, & OnCollisionEnter functions.
    
    Player shark has level, speed, health, and experience variables.

--- Hungry Shark RL/Assets/Scripts/PlayerShark.cs
+++ Hungry Shark RL/Assets/Scripts/PlayerShark.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShark : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerShark : MonoBehaviour {
+
+    //Values for the shark
+    [SerializeField] private float speed = 3.0f;
+    public int level {get; set;}
+    public int health { get; set; }
+    private int experience;
+
+    private void Awake() {
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        
+    }
+
+    void TakeDamage() {
+
+    }
+
+    void HealDamage() {
+
+    }
+
+    void LevelUp() {
+
     }
 }
