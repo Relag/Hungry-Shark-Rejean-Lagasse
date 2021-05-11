d40fe41054dcb49151e20d62df7fc22ec0a4d215
Relag <44652125+Relag@users.noreply.github.com>
2021-05-11 18:36:54 -0400

    Created Fish Classes
    Created HealthFish, GoodFish, and PoisonFish scripts all derived from a Fish Class.
    
    These fish will heal, level up, and hurt the shark respectively.

---
+++ Hungry Shark RL/Assets/Scripts/GoodFish.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodFish : Fish
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
