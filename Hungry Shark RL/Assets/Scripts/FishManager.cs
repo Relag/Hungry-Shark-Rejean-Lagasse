44d3749489a230861275400cb44d988b652b44b8
Relag <44652125+Relag@users.noreply.github.com>
2021-05-11 20:35:30 -0400

    Updated FishManager
    FishManager measures time since fish despawn and will respawn a fish at set intervals.
    
    FishManager now contains measurements for screen size.

--- Hungry Shark RL/Assets/Scripts/FishManager.cs
+++ Hungry Shark RL/Assets/Scripts/FishManager.cs
@@ -7,6 +7,17 @@ public class FishManager : MonoBehaviour
 
     public static FishManager instance;
     public List<Fish> pooledFish;
+    
+    //The world positions at the edge of the screen
+    private int top = 5;
+    private int bottom = -3;
+    private int left = -5;
+    private int right = 6;
+
+    //Timers for fish
+    private float fishTimer;
+    private float poisonFishTimer;
+    private float healthTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +26,36 @@ public class FishManager : MonoBehaviour
     }
 
     // Update is called once per frame
+    //Check if each fish is enabled. If not, check time lapsed since disable.
     void Update()
     {
-        
+        foreach (Fish fish in pooledFish) {
+            if (!fish.isActiveAndEnabled && fish.gameObject.CompareTag("Health")) {
+                healthTimer += Time.deltaTime;
+                if (healthTimer >= fish.spawnRate) {
+                    EnableFish(fish);
+                    healthTimer = 0f;
+                }
+            }
+            else if (!fish.isActiveAndEnabled && fish.gameObject.CompareTag("Fish")) {
+                fishTimer += Time.deltaTime;
+                if (fishTimer >= fish.spawnRate) {
+                    EnableFish(fish);
+                    fishTimer = 0f;
+                }
+            }
+            else if (!fish.isActiveAndEnabled && fish.gameObject.CompareTag("PoisonFish")) {
+                poisonFishTimer += Time.deltaTime;
+                if (poisonFishTimer >= fish.spawnRate) {
+                    EnableFish(fish);
+                    poisonFishTimer = 0f;
+                }
+            }   
+        }
+    }
+
+    void EnableFish(Fish fish) {
+        fish.timeSinceAppeared = 0f;
+        fish.gameObject.SetActive(true);
     }
 }
